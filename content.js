6f5e9ea807a69be25a3682d26b4c314495342f1a
Itone <y.itone07@gmail.com>
2025-07-27 21:49:13 +0900

    下層ページ更新

--- content.js
+++ content.js
@@ -85,6 +85,33 @@ $(window).scroll(function () {
 
 });
 
+// スクロール時のイベント
+$(window).scroll(function () {
+  // 画面がスクロールされた時に実行する
+
+  $(".contentfadein4").each(function () {
+    // fadeinクラスに対して順に処理を行う
+    // .each()：個別に処理を行うためのメソッド。繰り返し処理を行いながら各要素に対して操作を実行することができる。
+
+
+    // スクロールした距離
+    let scroll = $(window).scrollTop();
+    // 現在のスクロール位置を取得する。
+    // scrollTop()：要素のスクロール位置を取得
+
+    // fadeinクラスの要素までの距離
+    let target = $(this).offset().top;
+
+    // 画面の高さ
+    let windowHeight = $(window).height();
+
+    // fadeinクラスの要素が画面内にきたタイミングで要素を表示
+    if (scroll > target - windowHeight + 100) {
+      $(this).addClass('is-show');   // CSS だけでアニメ完結
+    }
+  });
+
+});
 
 // textanimation
 
